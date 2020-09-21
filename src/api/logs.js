15560d1dd261086ac38194b28b88c2f109eb1f09
Cheng <chengjohnsonhs@gmail.com>
2020-09-21 21:59:12 +0800

    server

---
+++ src/api/logs.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+/* eslint-disable quotes */
+const { Router } = require("express");
+const LogEntry = require("../models/LogEntry");
+
+const router = Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const entries = await LogEntry.find();
+    res.json(entries);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const logEntry = new LogEntry(req.body);
+    const createdEntry = await logEntry.save();
+    res.json(createdEntry);
+  } catch (err) {
+    if (err.name === "ValidationError") res.status(422);
+    next(err);
+  }
+
+  console.log(req.body);
+});
+
+module.exports = router;

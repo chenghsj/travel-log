634f7bf8027d2b83b74489e1349c0334183838c7
Cheng <chengjohnsonhs@gmail.com>
2020-09-23 13:32:38 +0800

    cors origin

--- client/src/API.js
+++ client/src/API.js
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:8001";
+// const API_URL = "http://localhost:8001";
+const API_URL = "https://travel-log-example.herokuapp.com";
 
 export const listLogEntries = async () => {
   const response = await fetch(`${API_URL}/api/logs`);
